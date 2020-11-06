899a9d9e974a35c1e91070ebce0c85d374385830
im karthikeyan.v (cyber._.spartan) <33289300+karthi-the-hacker@users.noreply.github.com>
2020-11-06 14:23:47 +0530

    Update m4skup.sh

--- m4skup.sh
+++ m4skup.sh
@@ -3,10 +3,10 @@ clear
 ./.banner
 read -p "Enter the full path with file name: " fl
 read -p "Enter company name: " comp
-mkdir ~/recon/$comp
+mkdir ~/recon/$comp &> /dev/null
 n=1
 cd ~/recon/$comp/
-mkdir m4skup
+mkdir m4skup &> /dev/null
 cd m4skup
 while read line;do
  #echo " Domain $n : $line"
@@ -15,3 +15,4 @@ while read line;do
  host $line |grep address | awk '{print "Domain:"$1 " --->  " "Ip Adress:" $NF}' >> domainwithip.txt
  n=$((n+1))
 done < $fl 
+cat ~/recon/$comp/m4skup/m4skup.txt | sort -u > m4skup-all.txt
